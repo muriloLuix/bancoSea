69bc603ef334df11dc846a6747d8f179b853f818
Murilo Luiz <muriloluiz654@gmail.com>
2024-06-14 16:09:35 -0300

    Adicionando mais propriedades ao JS

--- assets/js/switchTheme.js
+++ assets/js/switchTheme.js
@@ -1,24 +1,26 @@
 function switchTheme() {
     const body = document.body;
     const greenBg = document.querySelector('.greenBg');
-    const line = document.querySelector('.line')
+    const line = document.querySelectorAll('.line');
+    const menu = document.querySelector('.menu-expanded');
+    const menuLinks = document.querySelectorAll('.menu-link');
+    const conteudo = document.querySelector('.conteudo')
     const toggleSwitch = document.querySelector('.toggle-switch');
 
     toggleSwitch.addEventListener('click', () => {
         body.classList.toggle('bodyDark');
         body.classList.toggle('bodyWhite');
+        greenBg.classList.toggle('greenBgDark');
+        menu.classList.toggle('menu-expandedDark');
+        conteudo.classList.toggle('conteudoDark')
 
-        if (body.classList.contains('bodyDark')) {
-            greenBg.classList.add('greenBgDark');
-            greenBg.classList.remove('greenBg');
-            line.classList.add('lineWhite')
-            line.classList.remove('line')
-        } else {
-            greenBg.classList.add('greenBg');
-            greenBg.classList.remove('greenBgDark');
-            line.classList.add('line')
-            line.classList.remove('lineWhite')
-        }
+        menuLinks.forEach(link => {
+            link.classList.toggle('menu-linkWhite');
+        });
+
+        line.forEach(link => {
+            link.classList.toggle('lineWhite')
+        })
     });
 }
 
