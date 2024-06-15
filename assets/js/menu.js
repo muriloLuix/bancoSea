e01ddb6c902b6ea4df8c74d329cbacfb2fb2e81f
Murilo Luiz Jaboinski <muriloluiz654@gmail.com>
2024-06-15 01:41:32 -0300

    Ajustes na tela inicial
    Adicionado:
    
    Novas trocas de temas;
    Adicionado os serviços;
    Waves no meio da tela para divisão;

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,15 +1,34 @@
 export function toggle(element) {
-    element.classList.toggle("change");
+  element.classList.toggle("change");
 }
 
-// Abrir menu e fechar
-document.addEventListener('DOMContentLoaded', () => {
-    const hamburguerMenu = document.querySelector('.hamburguer-menu');
-    const container = document.querySelector('.container');
+// Abrir e fechar menu
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburguerMenu = document.querySelector(".hamburguer-menu");
+  const container = document.querySelector(".container");
+  const menuLinks = document.querySelectorAll(".menu-link");
+  const menuExpanded = document.querySelector(".menu-expanded");
 
-    function toggleMenu() {
-        toggle(container);
+  function toggleMenu() {
+    toggle(container);
+  }
+
+  function closeMenu() {
+    container.classList.remove("change");
+  }
+
+  hamburguerMenu.addEventListener("click", toggleMenu);
+
+  menuLinks.forEach((link) => {
+    link.addEventListener("click", closeMenu);
+  });
+
+  document.addEventListener("click", (event) => {
+    if (
+      !container.contains(event.target) &&
+      !hamburguerMenu.contains(event.target)
+    ) {
+      closeMenu();
     }
-
-    hamburguerMenu.addEventListener('click', toggleMenu);
+  });
 });
